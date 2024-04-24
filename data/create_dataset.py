6adbe0baade79e82f6a362ad108d4063b5898ab3
nsivaku <nsivaku@unc.edu>
2024-04-23 23:08:05 -0400

    update

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -3,8 +3,7 @@ import requests
 if __name__ == "__main__":
 
     urls = {
-        "search_channel": "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={0}&maxResults=1&order=date&type=video&key={1}",
-        "top10": "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={0}&maxResults=10&order=date&type=video&key={1}"
+        "top100": "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={0}&maxResults=100&order=date&type=video&key={1}"
     }
     
     top_10_videos_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCgoFStVyEsm8tBZP5NC-aBQ&maxResults=10&order=date&type=video&key=AIzaSyDnVwGFI4KcpzlkPTqSAcvuI-Ot8rEP0kw"
