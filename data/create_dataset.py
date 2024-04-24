aef49f50d41a6c703092a95f5e8ee69ada1811c9
nsivaku <nsivaku@unc.edu>
2024-04-23 23:18:29 -0400

    update

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -1,4 +1,13 @@
 import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the value of API_TOKEN environment variable
+api_token = os.getenv('API_TOKEN')
 
 if __name__ == "__main__":
 
@@ -6,5 +15,15 @@ if __name__ == "__main__":
         "top100": "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={0}&maxResults=100&order=date&type=video&key={1}"
     }
     
-    top_10_videos_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCgoFStVyEsm8tBZP5NC-aBQ&maxResults=10&order=date&type=video&key=AIzaSyDnVwGFI4KcpzlkPTqSAcvuI-Ot8rEP0kw"
-    "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=MrBeast&maxResults=1&order=date&type=video&key=AIzaSyDnVwGFI4KcpzlkPTqSAcvuI-Ot8rEP0kw"
+    df = pd.read_csv("data/top100.csv")
+    
+    for index, row in df.iterrows():
+        channel_id = row['channel_id']
+        top_100_videos_url = urls["top100"].format(channel_id, api_token)
+        response = requests.get(top_100_videos_url)
+        print(response.json())
+        break
+    
+    
+    
+    # top_10_videos_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCgoFStVyEsm8tBZP5NC-aBQ&maxResults=10&order=date&type=video&key=AIzaSyDnVwGFI4KcpzlkPTqSAcvuI-Ot8rEP0kw"
