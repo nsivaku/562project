22f424c1131b93ff7893d5350f676045db3e6543
nsivaku <nsivaku@unc.edu>
2024-05-02 21:53:49 -0400

    update

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -64,7 +64,3 @@ if __name__ == "__main__":
         # print(response.json())
 
     final_df.to_csv("data/top100_videos.csv")
-    
-    
-    
-    # top_10_videos_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCgoFStVyEsm8tBZP5NC-aBQ&maxResults=10&order=date&type=video&key=AIzaSyDnVwGFI4KcpzlkPTqSAcvuI-Ot8rEP0kw"
